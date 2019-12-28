f0c7b65b9202f1b1b099cf35ee1d997f1967e9ad
Christian <csoderba@gmail.com>
2019-12-28 19:59:25 +0200

    Added working implementation and tests
    Except for the print functions

--- src/Hash_LL.c
+++ src/Hash_LL.c
@@ -1,4 +1,5 @@
 #include "Hash_LL.h"
+#include <assert.h>
 
 typedef struct node
 {
@@ -8,9 +9,211 @@ typedef struct node
 
 typedef struct hashTable
 {
-	node **table;
+	node **array;
 	size_t valuesStored;
-	size_t buckets;
+	size_t bucketsUsed;
 	size_t bucketsMax;
-	size_t capacity;
 }hashTable;
+
+bool Hash_Create(table **ht, int buckets)
+{
+	if(!ht || buckets < 1)
+	{
+		return false;
+	}
+
+	hashTable *newTable = calloc(1, sizeof(hashTable));
+	if(!newTable)
+	{
+		return false;
+	}
+
+	newTable->bucketsMax = buckets;
+
+	node **newArray = calloc(newTable->bucketsMax, sizeof(node*));
+	if(!newArray)
+	{
+		free(newTable);
+		return false;
+	}
+
+	newTable->array = newArray;
+	*ht = newTable;
+
+	return true;
+}
+
+void Hash_Destroy(table **ht)
+{
+	if(!ht || !*ht)
+		return;
+
+	assert((*ht)->array);
+
+	size_t bucketsMax = (*ht)->bucketsMax;
+	node **array = (*ht)->array;
+
+	for(size_t i = 0; i < bucketsMax; ++i)
+	{
+		if(array[i])
+		{
+			node *curr = array[i];
+			node *prev;
+			while(curr)
+			{
+				prev = curr;
+				curr = curr->next;
+				free(prev);
+			}
+		}
+	}
+		
+	free(array);
+	free(*ht);
+
+	*ht = NULL;
+}
+
+enum {FIND, INSERT, DELETE};
+
+static bool find(table *ht, int value, int action)
+{
+	if(!ht)
+	{
+		return false;
+	}
+
+	assert(action == FIND || action == INSERT || action == DELETE);
+
+	size_t ind = value % ht->bucketsMax; // Calculate hash
+
+	node *curr = ht->array[ind];
+	node *prev = curr;
+
+	while(curr && value > curr->value)
+	{
+		prev = curr;
+		curr = curr->next;
+	}
+
+	if(curr && value == curr->value)
+	{
+		if(FIND == action)
+		{
+			return true; // Value found
+		}
+		else if(INSERT == action)
+		{
+			return false; // Duplicate value, do not insert
+		}
+		else // Delete value
+		{
+			node *deleteMe = curr;
+			if(prev == curr) // First entry in array
+			{
+				if((ht->array[ind] = curr->next) == NULL)
+				{
+					ht->bucketsUsed--;
+					assert(ht->bucketsUsed >= 0);
+				}
+			}
+			else
+			{
+				prev->next = curr->next;
+			}
+
+			free(deleteMe);
+			ht->valuesStored--;
+
+			return true;
+		}
+	}
+
+	if(FIND == action || DELETE == action)
+	{
+		return false; // Value not found
+	}
+
+	assert(action == INSERT);
+
+	node *newNode = calloc(1, sizeof(node));
+	if(!newNode)
+		return false;
+	newNode->value = value;
+
+	assert(curr == NULL || value < curr->value);
+
+	newNode->next = curr;
+
+	if(!prev)
+	{
+		assert(curr == NULL);
+		ht->array[ind] = newNode;
+		ht->bucketsUsed++;
+		assert(ht->bucketsUsed <= ht->bucketsMax);
+	}
+	else
+	{
+		prev->next = newNode;
+	}
+
+	ht->valuesStored++;
+
+	return true;
+}
+
+bool Hash_Insert(table *ht, int value)
+{
+	return find(ht, value, INSERT);
+}
+
+bool Hash_Exists(table *ht, int value)
+{
+	return find(ht, value, FIND);
+}
+
+bool Hash_Remove(table *ht, int value)
+{
+	return find(ht, value, DELETE);
+}
+
+size_t Hash_ValuesStored(table *ht)
+{
+	if(!ht)
+	{
+		return 0;
+	}
+
+	return ht->valuesStored;
+}
+
+size_t Hash_BucketsUsed(table *ht)
+{
+	if(!ht)
+	{
+		return 0;
+	}
+
+	return ht->bucketsUsed;
+}
+
+size_t Hash_BucketsMax(table *ht)
+{
+	if(!ht)
+	{
+		return 0;
+	}
+
+	return ht->bucketsMax;
+}
+
+double Hash_LoadFactor(table *ht)
+{
+	if(!ht)
+	{
+		return 0;
+	}
+
+	return (double)ht->bucketsUsed / ht->bucketsMax;
+}
+
--- tests/Hash_LL_Test.cpp
+++ tests/Hash_LL_Test.cpp
@@ -8,27 +8,382 @@ extern "C"
 #include "CppUTest/TestHarness.h"
 // https://github.com/cpputest/cpputest
 
-TEST_GROUP(Hash_LL_Test)
+TEST_GROUP(None)
 {
   	hashTable *ht;
 	
   	void setup()
   	{
-
+		CHECK(Hash_Create(&ht, 100));
+		CHECK(ht);
   	}
 
   	void teardown()
   	{
-
+		Hash_Destroy(&ht);
+		POINTERS_EQUAL(NULL, ht);
   	}
-
-  	#define SIZE(x) (sizeof(x)/sizeof(int))
 };
 
 #if 1
-TEST(Hash_LL_Test, Test)
+TEST(None, CreateFailsWhenPassingNullPointer)
 {
-	FAIL("Start here");
+	CHECK_FALSE(Hash_Create(NULL, 100));
+}
+#endif
+#if 1
+TEST(None, CreateFailsWhenPassingZeroOrNegativeSize)
+{
+	hashTable *t;
+	CHECK_FALSE(Hash_Create(&t, 0));
+	CHECK_FALSE(Hash_Create(&t, -1));
+}
+#endif
+#if 1
+TEST(None, CreateDoesNotCrash)
+{
+	hashTable *t;
+	CHECK(Hash_Create(&t, 100));
+	Hash_Destroy(&t);
+}
+#endif
+#if 1
+TEST(None, CreateAndDestroyAssignsToPointer)
+{
+	hashTable *t;
+	CHECK(Hash_Create(&t, 100));
+	CHECK(t);
+	Hash_Destroy(&t);
+	POINTERS_EQUAL(NULL, t);
+}
+#endif
+#if 1
+TEST(None, EmptyHasNoValuesAndNoBuckets)
+{
+	LONGS_EQUAL(0, Hash_ValuesStored(ht));
+	LONGS_EQUAL(0, Hash_BucketsUsed(ht));
+}
+#endif
+#if 1
+TEST(None, BucketsMaxDeterminedByInitialization)
+{
+	hashTable *t;
+	CHECK(Hash_Create(&t, 10));
+	LONGS_EQUAL(10, Hash_BucketsMax(t));
+	Hash_Destroy(&t);
+	CHECK(Hash_Create(&t, 10000));
+	LONGS_EQUAL(10000, Hash_BucketsMax(t));
+	Hash_Destroy(&t);
+}
+#endif
+#if 1
+TEST(None, LoadFactorIsZeroWhenEmpty)
+{
+	DOUBLES_EQUAL(0, Hash_LoadFactor(ht), 0.01);
+}
+#endif
+#if 1
+TEST(None, FunctionsReturnZeroWhenPassedNull)
+{
+	CHECK_FALSE(Hash_Insert(NULL, 1));
+	CHECK_FALSE(Hash_Exists(NULL, 1));
+	CHECK_FALSE(Hash_Remove(NULL, 1));
+	LONGS_EQUAL(0, Hash_ValuesStored(NULL));
+	LONGS_EQUAL(0, Hash_BucketsUsed(NULL));
+	LONGS_EQUAL(0, Hash_BucketsMax(NULL));
+	DOUBLES_EQUAL(0, Hash_LoadFactor(NULL), 0.0001);
 }
 #endif
 
+
+TEST_GROUP(One)
+{
+  	hashTable *ht;
+	
+  	void setup()
+  	{
+		CHECK(Hash_Create(&ht, 100));
+		CHECK(ht);
+  	}
+
+  	void teardown()
+  	{
+		Hash_Destroy(&ht);
+		POINTERS_EQUAL(NULL, ht);
+  	}
+};
+
+#if 1
+TEST(One, CanInsertOne)
+{
+	CHECK(Hash_Insert(ht, 1));
+}
+#endif
+#if 1
+TEST(One, InsertedValueExists)
+{
+	CHECK(Hash_Insert(ht, 1));
+	CHECK(Hash_Exists(ht, 1));
+}
+#endif
+#if 1
+TEST(One, CanRemoveInsertedValue)
+{
+	CHECK(Hash_Insert(ht, 1));
+	CHECK(Hash_Exists(ht, 1));
+	CHECK(Hash_Remove(ht, 1));
+}
+#endif
+#if 1
+TEST(One, OneValueOneBucket)
+{
+	CHECK(Hash_Insert(ht, 1));
+	LONGS_EQUAL(1, Hash_ValuesStored(ht));
+	LONGS_EQUAL(1, Hash_BucketsUsed(ht));
+}
+#endif
+#if 1
+TEST(One, ValueDoesNotExistAfterRemovingIt)
+{
+	CHECK(Hash_Insert(ht, 1));
+	CHECK(Hash_Exists(ht, 1));
+	CHECK(Hash_Remove(ht, 1));
+
+	CHECK_FALSE(Hash_Exists(ht, 1));
+	CHECK_FALSE(Hash_Remove(ht, 1));
+
+	LONGS_EQUAL(0, Hash_ValuesStored(ht));
+	LONGS_EQUAL(0, Hash_BucketsUsed(ht));
+}
+#endif
+#if 1
+TEST(One, CannotInsertTheSameValueTwice)
+{
+	CHECK(Hash_Insert(ht, 1));
+	CHECK_FALSE(Hash_Insert(ht, 1));
+	LONGS_EQUAL(1, Hash_ValuesStored(ht));
+	LONGS_EQUAL(1, Hash_BucketsUsed(ht));
+}
+#endif
+#if 1
+TEST(None, LoadFactorIsBucketsUsedDividedByBucketsMax)
+{
+	CHECK(Hash_Insert(ht, 1));
+	DOUBLES_EQUAL(1.0/100, Hash_LoadFactor(ht), 0.001);
+	DOUBLES_EQUAL(1.0/100, (double)Hash_BucketsUsed(ht)/Hash_BucketsMax(ht), 0.001);
+}
+#endif
+
+
+TEST_GROUP(Many)
+{
+  	hashTable *ht;
+  	#define SIZE 100
+	
+  	void setup()
+  	{
+		CHECK(Hash_Create(&ht, SIZE));
+		CHECK(ht);
+  	}
+
+  	void teardown()
+  	{
+		Hash_Destroy(&ht);
+		POINTERS_EQUAL(NULL, ht);
+  	}
+
+  	enum {NONE = 0, REVERSE = 1, NEGATE = 2};
+
+  	void insertMany(int howMany, int modifier)
+	{
+		if(!(modifier & REVERSE))
+		{
+			for(int i = 0; i < howMany; ++i)
+			{
+				if(!(modifier & NEGATE))
+					CHECK(Hash_Insert(ht, i));
+				else
+					CHECK_FALSE(Hash_Insert(ht, i));
+			}
+		}
+		else
+		{
+			for(int i = howMany - 1; i >= 0; --i)
+			{
+				if(!(modifier & NEGATE))
+					CHECK(Hash_Insert(ht, i));
+				else
+					CHECK_FALSE(Hash_Insert(ht, i));
+			}
+		}
+	}
+
+  	void existsMany(int howMany, int modifier)
+	{
+		if(!(modifier & REVERSE))
+		{
+			for(int i = 0; i < howMany; ++i)
+			{
+				if(!(modifier & NEGATE))
+					CHECK(Hash_Exists(ht, i));
+				else
+					CHECK_FALSE(Hash_Exists(ht, i));
+			}
+		}
+		else
+		{
+			for(int i = howMany - 1; i >= 0; --i)
+			{
+				if(!(modifier & NEGATE))
+					CHECK(Hash_Exists(ht, i));
+				else
+					CHECK_FALSE(Hash_Exists(ht, i));
+			}
+		}
+	}
+
+  	void removeMany(int howMany, int modifier)
+	{
+		if(!(modifier & REVERSE))
+		{
+			for(int i = 0; i < howMany; ++i)
+			{
+				if(!(modifier & NEGATE))
+					CHECK(Hash_Remove(ht, i));
+				else
+					CHECK_FALSE(Hash_Remove(ht, i));
+			}
+		}
+		else
+		{
+			for(int i = howMany - 1; i >= 0; --i)
+			{
+				if(!(modifier & NEGATE))
+					CHECK(Hash_Remove(ht, i));
+				else
+					CHECK_FALSE(Hash_Remove(ht, i));
+			}
+		}
+	}
+};
+
+#if 1
+TEST(Many, CanFillAllBuckets)
+{
+	insertMany(SIZE, NONE);
+	existsMany(SIZE, NONE);
+	LONGS_EQUAL(SIZE, Hash_ValuesStored(ht));
+	LONGS_EQUAL(SIZE, Hash_BucketsUsed(ht));
+	DOUBLES_EQUAL(1.00, Hash_LoadFactor(ht), 0.01);
+}
+#endif
+#if 1
+TEST(Many, DuplicatesRejected)
+{
+	insertMany(SIZE, NONE);
+	existsMany(SIZE, REVERSE);
+	insertMany(SIZE, NEGATE);
+	LONGS_EQUAL(SIZE, Hash_ValuesStored(ht));
+	LONGS_EQUAL(SIZE, Hash_BucketsUsed(ht));
+	DOUBLES_EQUAL(1.00, Hash_LoadFactor(ht), 0.01);
+}
+#endif
+#if 1
+TEST(Many, CanInsertTwiceAsManyValuesAsThereAreBuckets)
+{
+	insertMany(2 * SIZE, NONE);
+	LONGS_EQUAL(2 * SIZE, Hash_ValuesStored(ht));
+	LONGS_EQUAL(SIZE, Hash_BucketsUsed(ht));
+	DOUBLES_EQUAL(1.00, Hash_LoadFactor(ht), 0.01);
+}
+#endif
+#if 1
+TEST(Many, CanFillAndDeleteAllBuckets)
+{
+	insertMany(SIZE, NONE);
+	existsMany(SIZE, NONE);
+	removeMany(SIZE, NONE);
+
+	existsMany(SIZE, NEGATE);
+	removeMany(SIZE, NEGATE);
+
+	LONGS_EQUAL(0, Hash_ValuesStored(ht));
+	LONGS_EQUAL(0, Hash_BucketsUsed(ht));
+	DOUBLES_EQUAL(0, Hash_LoadFactor(ht), 0.01);
+}
+#endif
+#if 1
+TEST(Many, CanFillAndDeleteAllBucketsRepeatedly)
+{
+	for(int i = 0; i < 10; ++i)
+	{
+		insertMany(SIZE, REVERSE);
+		existsMany(SIZE, REVERSE);
+		removeMany(SIZE, REVERSE);
+		existsMany(SIZE, REVERSE | NEGATE);
+		removeMany(SIZE, REVERSE | NEGATE);
+		LONGS_EQUAL(0, Hash_ValuesStored(ht));
+		LONGS_EQUAL(0, Hash_BucketsUsed(ht));
+		DOUBLES_EQUAL(0, Hash_LoadFactor(ht), 0.01);
+	}
+}
+#endif
+#if 1
+TEST(Many, HowNotToMakeATestCase)
+{
+	for(int i = 0; i < 3 * SIZE; i += 2) 		// Add even values
+	{
+		CHECK(Hash_Insert(ht, i));
+	}
+	for(int i = 0; i < 3 * SIZE; i += 2)
+	{
+		CHECK(Hash_Exists(ht, i)); 				// Even values exist
+		CHECK_FALSE(Hash_Exists(ht, i + 1)); 	// Odd values do not exist
+	}
+	for(int i = 0; i < 3 * SIZE; i += 2)
+	{
+		CHECK(Hash_Insert(ht, i + 1)); 			// Add odd values
+	}
+	for(int i = 0; i < 3 * SIZE; i += 2)
+	{
+		CHECK(Hash_Exists(ht, i)); 				// Even values exist
+		CHECK(Hash_Exists(ht, i + 1)); 			// Odd values exist
+	}
+	for(int i = 0; i < 3 * SIZE; i += 2)
+	{
+		CHECK(Hash_Remove(ht, i + 1)); 			// Remove odd values
+	}
+	for(int i = 0; i < 3 * SIZE; i += 2)
+	{
+		CHECK(Hash_Exists(ht, i));  			// Even values exist
+		CHECK_FALSE(Hash_Exists(ht, i + 1));  	// Odd values do not exist
+	}
+	for(int i = 0; i < 3 * SIZE; i += 2)
+	{
+		CHECK(Hash_Insert(ht, i + 1)); 			// Add odd values
+	}
+	for(int i = 0; i < 3 * SIZE; i += 2)
+	{
+		CHECK(Hash_Exists(ht, i)); 				// Even values exist
+		CHECK(Hash_Exists(ht, i + 1)); 			// Odd values exist
+	}
+	for(int i = 0; i < 3 * SIZE; i += 2)
+	{
+		CHECK(Hash_Remove(ht, i)); 				// Remove even values
+	}
+	for(int i = 0; i < 3 * SIZE; i += 2)
+	{
+		CHECK_FALSE(Hash_Exists(ht, i));		// Even values do not exist
+		CHECK(Hash_Exists(ht, i + 1));			// Odd values exist
+	}
+	for(int i = 0; i < 3 * SIZE; i += 2)
+	{
+		CHECK(Hash_Remove(ht, i + 1)); 			// Remove odd values
+	}
+	for(int i = 0; i < 3 * SIZE; i += 2)
+	{
+		CHECK_FALSE(Hash_Exists(ht, i));		// Even values do not exist
+		CHECK_FALSE(Hash_Exists(ht, i + 1));	// Odd values do not exist
+	}
+}
+#endif
